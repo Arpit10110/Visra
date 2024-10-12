1518b5df456af3511d9640fca284323e0d71bcd8
Arpit <omagrahari55@gmail.com>
2024-10-12 17:07:23 +0530

    contact page redesign

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -11,6 +11,7 @@ import  instagram from "@/assets/instagram.png"
 import  facebook from "@/assets/facebook.png"
 import  linkedin from "@/assets/linkedin.png"
 import  twitter from "@/assets/twitter.png"
+import  contactUs from "@/assets/contact-us-banner.png"
 import Image from "next/image";
 const page = () => {
     const [state, handleSubmit] = useForm("mbljrvae");
@@ -21,13 +22,16 @@ const page = () => {
   return (
    <>
    <Navbar/>
-   <div className="flex justify-center pt-[2.5rem] w-full pb-[10rem]">
-    <div className='w-full flex flex-col justify-center items-center gap-[1rem] ' >
-        <div className='flex w-[50%] justify-between below-sm:w-full pl-[1rem] ' >
-            <div className='w-[45%] text-[2rem] below-sm:text-[1.3rem] ' >
+   <div className="pb-[10rem]">
+    <div className='w-full' >
+        <Image className='w-full' src={contactUs} alt="ContactUs"  />
+    </div>
+    <div className='below-sm:w-[90%] below-sm:top-0 below-sm:p-0    w-[50%] flex flex-col justify-center items-center gap-[1rem] m-auto bg-white relative top-[-5rem] rounded-[1rem] p-[2rem]' >
+        <div className='flex w-[100%] items-center  justify-between below-sm:w-full gap-[2rem] below-sm:pl-[1rem] ' >
+            <div className='w-[50%] text-[2rem] below-sm:text-[1.3rem] ' >
                 <h2 className='font-semibold' >Love to hear from you, Get in touch👋</h2>
             </div>
-            <div className='w-[45%] text-[1.5rem] gap-[1rem] flex flex-col below-sm:text-[1.2rem]' >
+            <div className='w-[50%] text-[1.5rem] gap-[1rem] flex flex-col below-sm:text-[1.2rem]' >
                 <h2 className='font-semibold' >Follow Our Social Media</h2>
                 <div className='w-[100%] flex gap-[1rem] ' >
                     <Image className='w-[10%]  ' src={instagram} alt="Instagram"  />
@@ -37,20 +41,33 @@ const page = () => {
                 </div>
             </div>
         </div>
-        <div className='w-full justify-center flex' >
-            <form className=" w-[50%]   rounded-md  below-sm:w-[95%] below-sm:p-[1rem] flex flex-col gap-[1rem] items-start "   id='myForm' onSubmit={handleSubmit}>
+        <div className='w-full ' >
+            <form className=" w-[100%]   rounded-md  below-sm:w-[95%] below-sm:p-[1rem] flex flex-col gap-[1rem] items-start "   id='myForm' onSubmit={handleSubmit}>
                 <div className=" flex gap-[2rem] w-full  ">
-                    <input className='bg-gray-200  w-[50%] p-[0.4rem] text-[1.2rem] rounded-[10px] ' type="text" name='FirstName' placeholder='First Name' required/>
-                    <input className='bg-gray-200 w-[50%] p-[0.4rem] text-[1.2rem] rounded-[10px] ' type="text" name='LastName' placeholder='Last Name' required />
+                    <div className='w-[50%]' >
+                    <h2>First Name</h2>
+                    <input className='bg-gray-50  w-full p-[0.4rem] text-[1.2rem] rounded-[10px] ' type="text" name='FirstName' required/>
+                    </div>
+                    <div className='w-[50%]' >
+                        <h2>Last Name</h2>
+                    <input className='bg-gray-50 w-full p-[0.4rem] text-[1.2rem] rounded-[10px] ' type="text" name='LastName'  required />
+                    </div>
                 </div>
                 <div className=" flex gap-[2rem] w-full  ">
-                    <input className='bg-gray-200 w-[50%] p-[0.4rem] text-[1.2rem] rounded-[10px] ' type="text" name='Email' placeholder='Email address' required/>
-                    <input className='bg-gray-200 w-[50%] p-[0.4rem] text-[1.2rem] rounded-[10px] ' type="text" name='Phone' placeholder='Phone number'  required/>
+                <div className='w-[50%]' >
+                        <h2>Your Email</h2>
+                        <input className='bg-gray-50 w-full p-[0.4rem] text-[1.2rem] rounded-[10px] ' type="text" name='Email' required/>
+                    </div>
+                    <div className='w-[50%]' >
+                        <h2>Your Phone no.</h2>
+                    <input className='bg-gray-50 w-full p-[0.4rem] text-[1.2rem] rounded-[10px] ' type="text" name='Phone'  required/>
+                    </div>
                 </div>
                 <div className="w-full ">
-                    <input className='bg-gray-200 w-full h-[30vh] text-center p-[0.2rem] text-[1.2rem] rounded-[10px] ' type="text" name='Message' placeholder='Message'  required/>
+                    <h2>Message</h2>
+                    <input className='bg-gray-50 w-full h-[30vh] text-center p-[0.2rem] text-[1.2rem] rounded-[10px] ' type="text" name='Message'   required/>
                 </div>
-                <button className='bg-gray-800 p-[0.6rem] text-[1.1rem] font-semibold text-white rounded-[1rem] cursor-pointer border-none ' >Send Messgae</button>
+                <button className='bg-gray-800 p-[0.6rem] w-fit p-l-[1rem] pr-[1rem] text-center text-[1.1rem] font-semibold text-white  cursor-pointer border-none ' >Send Messgae</button>
             </form>
         </div>
     </div>
