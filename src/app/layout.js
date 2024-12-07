506fee0801a47612f456f7dabe0ba412d70f02eb
Arpit <omagrahari55@gmail.com>
2024-12-07 19:03:59 +0530

    favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import ReduxProvider from "@/provider";
 export const metadata = {
   title: "Visra",
   description: "DIGITAL SIGNATURES CERTIFICATE",
+  icons :{
+    icon:"https://visra.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.967247f3.png&w=3840&q=75",
+    apple:"https://visra.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.967247f3.png&w=3840&q=75"
+  }
 };
 export default function RootLayout({ children }) {
   return (
